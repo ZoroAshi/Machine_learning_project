35a8dcbd24e1d6a2c6381909d3d9269a3a390569
ZoroAshi <idelapoterie@hotmail.fr>
2021-11-17 18:14:15 +0100

    Update 21-11-08_project1_1.py

--- 21-11-08_project1_1.py
+++ 21-11-08_project1_1.py
@@ -72,6 +72,8 @@ for k in range(len(x0)):
 		xk =  x0[k]
 		X0.append(xk)
 
+matrix[:,1] = X0
+
 # Third column
 
 Y0 = []
@@ -80,8 +82,54 @@ for k in range(len(y0)) :
 		yk = x0[i]
 		Y0.append(yk)
 
+matrix[:,2] = Y0
+
+# Fourth column
+
+X02 = []
+for k in range(len(x0)**2):
+	X02.append(X0[k]**2)
+
+matrix[:,3] = X02
+
+# Fith column
+
+Y02 = []
+for k in range(len(y0)**2):
+	Y02.append(Y0[k]**2)
+	
+matrix[:,4] = Y02
+
+# six column
+
+X0Y0 = []
+for k in range(len(x0)*len(y0)):
+	X0Y0.append(X0[k]*Y0[k])
+	
+matrix[:,5] = X0Y0
+
+
+# print(matrix)
+
+
 
 # Tests
 
+def create_X(x, y, n ):
+	if len(x.shape) > 1:
+		x = np.ravel(x)
+		y = np.ravel(y)
+	N = len(x)
+	l = int((n+1)*(n+2)/2) # Number of elements in beta
+	X = np.ones((N,l))
+	for i in range(1,n+1):
+		q = int((i)*(i+1)/2)
+		for k in range(i+1):
+			X[:,q+k] = (x**(i-k))*(y**k)
+	return X
 
-
+X = create_X(x,y,2)
+print(matrix)
+print(len(matrix))
+print(X)
+print(len(X))
