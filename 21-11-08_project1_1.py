c4e41b2d8d8d9a41a4d3eac142ae7fec0bbb4693
HugoDelille <83717746+HugoDelille@users.noreply.github.com>
2021-12-08 15:31:49 +0100

    Update 21-11-08_project1_1.py

--- 21-11-08_project1_1.py
+++ 21-11-08_project1_1.py
@@ -12,6 +12,7 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from sklearn.model_selection import train_test_split
 
 import pandas as pd
 import numpy as np
