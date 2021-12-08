92995de2e3c31cd1da75099e32a1de6293948962
HugoDelille <83717746+HugoDelille@users.noreply.github.com>
2021-12-08 15:55:53 +0100

    Update project1_2.py
    Copie du project_3 pour rendre plus complet, et retirer les choses non nécessaires à la Q2

--- project1_2.py
+++ project1_2.py
@@ -8,8 +8,10 @@ import sklearn.linear_model as skl
 import pandas as pd
 import numpy as np
 from random import random, seed
+
 def noise():
-    noise=np.random.normal(0,1)
+    seed_noise=np.random.RandomState(314159)
+    noise=seed_noise.normal(0,1)
 
     return noise
 
@@ -24,9 +26,11 @@ def FrankeFunction(x,y):
 
 
 noise_z=noise()
-N=1000
-x=np.sort(np.random.uniform(0,1,N))
-y=np.sort(np.random.uniform(0,1,N))
+N=10000
+seed_x=np.random.RandomState(123456)
+seed_y=np.random.RandomState(654321)
+x=np.sort(seed_x.uniform(0,1,N))
+y=np.sort(seed_y.uniform(0,1,N))
 
 z = FrankeFunction(x,y)+noise_z
 
@@ -45,43 +49,65 @@ def create_X(x, y, n ):
 	return X
 
 complexity=[]
-r2_test,r2_train=[],[]
-mse_test,mse_train=[],[]
+r2_test,r2_train=[0]*30,[0]*30
+r2_test_,r2_train_=[],[]
+mse_test,mse_train=[0]*30,[0]*30
+mse_test_,mse_train_=[],[]
 
 
+    #bootstrap
+k=1000
+order=10
+for i in range (order):
+    complexity.append(i)
 
+for i in range (k):
 
-for n in range(30):
-    X = create_X(x,y,n)
-    """print(matrix)
-    print(len(matrix))
-    print(X)
-    print(len(X))"""
+    
+    for n in range(order):
+        X = create_X(x,y,n)
+    
+        X_train,X_test,z_train,z_test=train_test_split(X,z,test_size=0.2)
+        beta=np.linalg.pinv(X_train.T@ X_train)@X_train.T@z_train
+        z_tild=X_train@beta
+        z_predict=X_test@beta
+        R2_test=r2_score(z_test,z_predict)
+        R2_train=r2_score(z_train,z_tild)
+        MSE_test=mean_squared_error(z_test,z_predict)
+        MSE_train=mean_squared_error(z_train,z_tild)
+        
+        r2_test_.append(R2_test)
+        r2_train_.append(R2_train)
+        mse_test_.append(MSE_test)
+        mse_train_.append(MSE_train)
+        
+    r2_test=[r2_test+r2_test_ for r2_test,r2_test_ in zip(r2_test,r2_test_)]
+    r2_train=[r2_test+r2_test_ for r2_test,r2_test_ in zip(r2_train,r2_train_)]
+    mse_test=[r2_test+r2_test_ for r2_test,r2_test_ in zip(mse_test,mse_test_)]
+    mse_train=[r2_test+r2_test_ for r2_test,r2_test_ in zip(mse_train,mse_train_)]
 
- 
-    X_train,X_test,z_train,z_test=train_test_split(X,z,test_size=0.2)
-    beta=np.linalg.pinv(X_train.T@ X_train)@X_train.T@z_train
-    z_tild=X_train@beta
-    z_predict=X_test@beta
-    R2_test=r2_score(z_test,z_predict)
-    R2_train=r2_score(z_train,z_tild)
-    MSE_test=mean_squared_error(z_test,z_predict)
-    MSE_train=mean_squared_error(z_train,z_tild)
+r2_test  =[i*(1/k) for i in r2_test]
+r2_train =[i*(1/k) for i in r2_train]
+mse_test =[i*(1/k) for i in mse_test]
+mse_train=[i*(1/k) for i in mse_train]
 
-    complexity.append(n)
-    r2_test.append(R2_test)
-    r2_train.append(R2_train)
-    mse_test.append(MSE_test)
-    mse_train.append(MSE_train)
-    """
-    print("MSE train ={} ".format(MSE_train))
-    print("MSE test  ={} ".format(MSE_test))
-    print("R2 train  ={} ".format(R2_train))
-    print("R2 test   ={} ".format(R2_test))
-    """
+print("MSE train true ={} ".format(mse_train[5]))
+print("MSE test true  ={} ".format(mse_test[5]))
+print("R2 train true  ={} ".format(r2_train[5]))
+print("R2 test true   ={} ".format(r2_test[5]))
+    
+  
 plt.figure(1)
 plt.plot(complexity,mse_test,label='MSE test')
 plt.plot(complexity,mse_train,label='MSE train')
 #plt.axis([0,30,0,20])
 plt.legend()
+
+plt.figure(2)
+plt.plot(complexity,r2_test,label='R2 test')
+plt.plot(complexity,r2_train,label='R2 train')
+#plt.axis([0,30,0,20])
+plt.legend()
+
+
 plt.show()
